4a8b508316a3a4a1a6673b186fe2961d8e07c203
AlexeyDevTech <pan@angstrem.tech>
2022-09-05 21:57:57 +0300

    Initial commit

---
+++ SQLiteDBCreator/VMs/MainWindowViewModel.cs
@@ -0,0 +1,32 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace SQLiteDBCreator.VMs
+{
+    public class MainWindowViewModel : BindableBase
+    {
+        public ICommand ExitCommand { get; set; }
+        public ICommand AboutProgrammCommand { get; set; }
+        public MainWindowViewModel()
+        {
+            ExitCommand = new DelegateCommand(ExitAction);
+            AboutProgrammCommand = new DelegateCommand(AboutProgrammAction);
+        }
+
+        private void AboutProgrammAction()
+        {
+            //about programm window...
+        }
+
+        private void ExitAction()
+        {
+            App.Current.Shutdown();
+        }
+    }
+}
